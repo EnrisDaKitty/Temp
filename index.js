3c82c5673c35198192be5019ceb4b80fcd6f462d
Naven Rosales <nrosales@student.neumont.edu>
2023-09-05 01:22:22 -0600

    ffgh

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import { Express } from "express";
+import express from "express";
 
-const app = Express()
+const app = express()
 
 app.get('/', (req,res) => {
     res.send('Express vercel app response')
